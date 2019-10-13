8e75fedeeea41a32e3c35c609303490fe2ec85c6
vadim <Vadstr23@gmail.com>
2019-10-13 11:50:51 +0300

    стулья

---
+++ the session/DynamicOBJ.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicOBJ : MonoBehaviour
+{
+    public GameObject Stul;
+    public Vector2 Vectorr;
+
+    private void Start()
+    {
+        loadPosition();
+        Application.targetFrameRate = 60;
+        
+    }
+
+
+    void Update()
+    {
+        savePosition();
+        loadPosition();
+    }
+
+    public void savePosition()
+        {
+
+            Transform CurrentPlayerPosition = this.gameObject.transform;
+
+            PlayerPrefs.SetFloat("PosX1", CurrentPlayerPosition.position.x);
+            PlayerPrefs.SetFloat("PosY1", CurrentPlayerPosition.position.y);
+        }
+
+        public void loadPosition()
+        {
+
+            Transform CurrentPlayerPosition = this.gameObject.transform;
+
+            Vector2 PlayerPosition = new Vector2(PlayerPrefs.GetFloat("PosX1"),
+                        PlayerPrefs.GetFloat("PosY1"));
+
+            CurrentPlayerPosition.position = PlayerPosition;
+        }
+    }
